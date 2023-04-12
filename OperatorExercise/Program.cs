103d9830065f56697934bea9d8592275e945ec70
Hamisi Ngao <newhamisi@gmail.com>
2023-04-11 23:02:12 -0400

    complete ex 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 10;
+            int y = 5;
+
+            int a = 17;
+            int b = 4;
+            int quotient = a/b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"{x} + {y} is: {x + y}");
+            Console.WriteLine($"{x} - {y} is: {x - y}");
+            Console.WriteLine($"{x} * {y} is: {x * y}");
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+
         }
     }
 }
