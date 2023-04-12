55998989cba2d40d30e6f55229eb3d7fcd6a244b
Hamisi Ngao <newhamisi@gmail.com>
2023-04-11 23:18:23 -0400

    complete ex 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,6 +2,14 @@
 {
     public class Program
     {
+        public static void CalculateAreaOfCircle(){
+            Console.WriteLine("what is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of a circle with radius {radius} is {area}");
+            Console.WriteLine("Press any key to continue...");
+        }
         static void Main(string[] args)
         {
             int x = 10;
@@ -18,6 +26,8 @@
             Console.WriteLine($"{x} * {y} is: {x * y}");
 
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            CalculateAreaOfCircle();
 
 
 
